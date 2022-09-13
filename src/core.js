66c3af2e247b382c5060934e61b0f6d1ea9da38d
Matias Nuñez <Matiasne45@gmail.com>
2022-09-13 20:14:49 -0300

    Modify port declaration

--- src/core.js
+++ src/core.js
@@ -87,7 +87,7 @@ export async function session(name, conversation) {
  * @param {String} name
  * @param {Number} port
  */
-export async function httpCtrl(name, port = 3000) {
+export async function httpCtrl(name, port) {
   if (!fs.existsSync("logs")) {
     fs.mkdirSync("logs", { recursive: true });
     fs.writeFileSync("logs/conversations.log", "");
